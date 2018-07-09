e4a53102495a460217dfc7e5c39cbf8a623779d7
EricChows <xnzds2008@163.com>
2018-07-09 15:04:22 +0800

    Update NotificationPanelView.java
    set default debug switch to false

--- 8.1.0/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ 8.1.0/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -81,7 +81,7 @@ public class NotificationPanelView extends PanelView implements
         KeyguardAffordanceHelper.Callback, NotificationStackScrollLayout.OnEmptySpaceClickListener,
         OnHeadsUpChangedListener, QS.HeightListener {
 
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
 
     public static final String TAG = NotificationPanelView.class.getSimpleName();
 
